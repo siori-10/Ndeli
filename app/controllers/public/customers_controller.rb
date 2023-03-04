6e38ba93bc9a29356ce34736d853c5199f502b1f
EC2 Default User <ec2-user@ip-172-31-41-89.ap-northeast-1.compute.internal>
2023-03-04 22:25:36 +0000

    [ADD]会員情報編集画面追加、退会画面追加

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -9,15 +9,24 @@ class Public::CustomersController < ApplicationController
   end
 
   def edit
+    @customer= current_customer
   end
 
   def update
+    @customer= current_customer
+    @customer.update(customer_params)
+    redirect_to customers_my_page_path
   end
 
   def unsubscribe
+    
   end
 
   def withdraw
+    @customer=current_customer
+    @customer.update(is_deleted:true)
+    reset_session
+    redirect_to root_path
   end
   
    
