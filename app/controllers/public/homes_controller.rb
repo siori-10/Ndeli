c161c9b57c5fcce01915811b36bca2b9612ff6e6
EC2 Default User <ec2-user@ip-172-31-41-89.ap-northeast-1.compute.internal>
2023-04-12 12:10:55 +0000

    [ADD]topページのレシピ一覧に下書き保存したレシピを公開しないようにした

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,6 +1,6 @@
 class Public::HomesController < ApplicationController
   def top
-    @recipes = Recipe.all.order(created_at: :desc).page(params[:page]).per(3)
+    @recipes = Recipe.where(is_draft: false).order(created_at: :desc).page(params[:page]).per(3)
   end
 
   def about
